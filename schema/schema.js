6f865ff5831d1b25245b5eb7c3e79299d64c9cd4
Gianfranco <gianffh@gmail.com>
2020-08-02 12:13:49 +0800

    ! Add axios and connect to schema user

--- schema/schema.js
+++ schema/schema.js
@@ -1,4 +1,5 @@
 const graphql = require('graphql');
+const axios = require('axios');
 const {
   GraphQLObjectType,
   GraphQLString,
@@ -21,7 +22,8 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString }},
       resolve(parentValue, args) {
-        return {};
+        return axios.get(`http://localhost:3000/users/${args.id}`)
+          .then(response => response.data);
       }
     }
   }
